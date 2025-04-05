3f8ea6b5f3522fd6b7db8fbfbf80ba495cded991
Anurag Kuche <anuragkuche1@gmail.com>
2025-04-05 23:43:33 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,7 +4,7 @@ const Login = () => {
   return (
     <div className="container">
       <h2>Signin to your PopX account</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+      <p>Get started with a seamless and secure registration process</p>
       <input type="email" placeholder="Enter email address" />
       <input type="password" placeholder="Enter password" />
       <button className="btn disabled">Login</button>
